a75c9309a9a59e51014d00c34fd7a18dfc655294
ekm21 <96369319+ekm21@users.noreply.github.com>
2021-12-21 23:01:20 +0100

    react starter code

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+
+const getData=()=>{
+  
+}
+
+  return (
+    <div calssName='app'>
+     <p>app</p>
+     <button onClick={getData}>GET TASKS</button>
+    </div>
+  );
+}
+
+ 
