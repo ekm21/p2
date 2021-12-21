656c7abbbfb12840b4911e8b9710422d1fb2e0df
ekm21 <96369319+ekm21@users.noreply.github.com>
2021-12-22 00:35:28 +0100

    finish useEffict and map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios'
+import Todo from './components/Todo'
+
 
 export default function App() {
+const [tasks, setTasks] = useState([])
+
+useEffect(()=>{
+getData()
+},[])
 
 const getData=()=>{
-  
-}
+  // should bring data using axios
+  // from backend (GET /tasks)
+  axios
+  .get('http://localhost:5000/tasks')
+  .then((response)=>{
+  // console.log('RESPONSE: ',response);
+  console.log('DATA: ',response.data);
+  setTasks(response.data)
+})
+.catch((err)=>{
+  console.log('ERR: ',err);
+})
+};
+
+const mapOverTasks=tasks.map((taskObj,i) => (
+<Todo key={i} task={taskObj}/>
+));
+
 
   return (
-    <div calssName='app'>
+    <div className='app'>
      <p>app</p>
+
+     {/* when click on this button
+      should call function bring Data*/}
      <button onClick={getData}>GET TASKS</button>
+
+     {mapOverTasks}
     </div>
   );
 }
